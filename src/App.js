f1630058db66c3ef1a6627c9282d9ab5f41bfd64
PauloAlmeidaBrasa <almeida.desenvolve@gmail.com>
2024-10-21 14:08:41 -0300

    feat: add dynamic to render sign or pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import MarketingPage from './marketing-page/MarketingPage';
+import SignIn from './sign-in/SignIn'
 
 function App() {
+
+
+  const userCookie = getCookie('yourCookieName');
+
   return (
     <div className="App">
-      <MarketingPage></MarketingPage>
+      
+       {/* Conditionally render SignIn or MarketingPage based on the presence of the cookie */}
+       {userCookie ? <MarketingPage /> : <SignIn />}
+
+
+      {/* <MarketingPage></MarketingPage> */}
     </div>
   );
 }
+
+// Utility function to get a cookie by name
+const getCookie = (cookieName) => {
+  const name = cookieName + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i].trim();
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return null;
+};
 
 export default App;
